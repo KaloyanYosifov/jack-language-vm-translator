edd2cc027bc1b0a97613acbecec92b4e3da7632c
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-11-23 20:40:21 +0200

    Update test

--- tests/Unit/Converter/SegmentToAssemblerNameConvertorTest.php
+++ tests/Unit/Converter/SegmentToAssemblerNameConvertorTest.php
@@ -5,64 +5,22 @@ namespace Tests\Unit\Converter;
 use JackVMTranslator\Enums\MemorySegment;
 use JackVMTranslator\Converter\SegmentToAssemblerNameConvertor;
 
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'datasets.php';
+
 it('it converts memory segment to assembler variable', function ($segment, $fileName, $location, $expectation) {
     $convertor = new SegmentToAssemblerNameConvertor();
     $converted = $convertor->convert($segment, $fileName, $location);
     expect($converted)->toEqual($expectation);
 })
-    ->with([
-        [
-            MemorySegment::LOCAL_SEGMENT(),
-            '',
-            0,
-            'LCL',
-        ],
-        [
-            MemorySegment::ARGUMENT_SEGMENT(),
-            '',
-            0,
-            'ARG',
-        ],
-        [
-            MemorySegment::THIS_SEGMENT(),
-            '',
-            0,
-            'THIS',
-        ],
-        [
-            MemorySegment::THAT_SEGMENT(),
-            '',
-            0,
-            'THAT',
-        ],
-        [
-            MemorySegment::CONSTANT_SEGMENT(),
-            '',
-            55,
-            '55',
-        ],
-        [
-            MemorySegment::TEMP_SEGMENT(),
-            '',
-            5,
-            'R10',
-        ],
-        [
-            MemorySegment::STATIC_SEGMENT(),
-            'Gangster.txt',
-            5,
-            'Gangster.5',
-        ],
-        [
-            MemorySegment::POINTER_SEGMENT(),
-            '',
-            0,
-            'THIS',
-        ],
-        [
-            MemorySegment::POINTER_SEGMENT(),
-            '',
-            1,
-            'THAT',
-        ],
-    ]);
+    ->with('memory_segment_for_conversion');
+
+it(
+    'throws an error when temp variable is out of bounds',
+    fn() => (new SegmentToAssemblerNameConvertor())->convert(MemorySegment::TEMP_SEGMENT(), '', 13)
+)
+    ->throws(\LogicException::class, 'Temporary variable out of scope!');
+it(
+    'throws an error when pointer variable is out of bounds',
+    fn() => (new SegmentToAssemblerNameConvertor())->convert(MemorySegment::POINTER_SEGMENT(), '', 2)
+)
+    ->throws(\LogicException::class, 'Pointer out of scope, please use 1 or 0.');
